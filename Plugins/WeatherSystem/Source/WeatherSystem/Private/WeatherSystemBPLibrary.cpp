9b00036ba579ec31b6e82d9640624f6cd6e850d8
CandyFreak2 <TheCandyFreak2@gmail.com>
2026-02-08 15:30:13 -0500

    rain and base weather system

---
+++ Plugins/WeatherSystem/Source/WeatherSystem/Private/WeatherSystemBPLibrary.cpp
@@ -0,0 +1,16 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "WeatherSystemBPLibrary.h"
+#include "WeatherSystem.h"
+
+UWeatherSystemBPLibrary::UWeatherSystemBPLibrary(const FObjectInitializer& ObjectInitializer)
+: Super(ObjectInitializer)
+{
+
+}
+
+float UWeatherSystemBPLibrary::WeatherSystemSampleFunction(float Param)
+{
+	return -1;
+}
+
